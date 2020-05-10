94de4c9ab7a1f712e5557d83c10c402e20dedd68
Gabriele Battaglia <gabriele1.battaglia@mail.polimi.it>
2020-05-10 14:05:18 +0200

    ubknn

---
+++ project2/data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+f = open("ml-100k/u.data","r") 
+
+lines = f.readlines()
+
+R = np.empty((1000,17000))  
+R[:] = np.nan
+
+for i, line in enumerate(lines):
+ print(i)
+ s = line.split()[:3] 
+ R[int(s[0]), int(s[1])] = int(s[2])
+
+
+df = pd.DataFrame(R)
+ 
+df.to_csv("data.csv", index = True)
---
+++ project2/ubknn.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+import math
+
+class ubknn():
+ def __init__(self, RIn, k):
+  self.k = k
+  
+  self.Rsparse = pd.DataFrame(RIn)
+  
+  self.R = self.__fillNaN(self.Rsparse)
+  
+  self.S = self.__similarityMatrix(self.R)
+ 
+ 
+ 
+ def __fillNaN(self, df):
+  #fill with average rating of user
+  return(df.fillna(df.mean(axis=1)).fillna(df.mean(axis=0)).fillna(0))
+ 
+ 
+ 
+ def __similarityMatrix(self, df):
+  cs = cosine_similarity(df)
+  np.fill_diagonal(cs, 0)
+  return(pd.DataFrame(cs,index = df.index))
+ 
+ 
+ 
+ def __kNNs(self, u, k):
+  return(list(self.S.sort_values([u], ascending = False).head(k).index))
+ 
+ 
+ 
+ def __generateScore(self, u, m):
+  neighbours = self.__kNNs(u, self.k)
+  
+  similarities = np.array(self.S.iloc[u, neighbours])
+  rel_rating = np.array(self.R.iloc[neighbours, m]) - np.array(self.__averageScore(neighbours))
+  
+  score = self.__averageScore(u) + np.sum(rel_rating*similarities)/np.sum(similarities)
+  
+  return(score)
+ 
+ 
+ 
+ def __averageScore(self, u):
+  return(self.R.iloc[:,u].mean(axis=0))
+ 
+ 
+ 
+ def score(self, u, m, forceGen=True):
+  if((not math.isnan(self.Rsparse.iloc[u,m])) and (not forceGen)):
+   return(self.Rsparse.iloc[u,m])
+  else:
+   return(self.__generateScore(u, m))
+ 
+ 
+ def setK(k):
+  self.k = k
+
+
+
+
+model = ubknn(df, 10)
+model.score(1,1)
+model.score(2,2)
+
